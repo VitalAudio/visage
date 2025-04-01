2c3033628528a41d470e4b3e10717998075ac019
Matt Tytel <matt@vital.audio>
2025-04-01 13:49:24 -0400

    Triangulating non-intersecting paths works.

--- examples/Basic/basic.cpp
+++ examples/Basic/basic.cpp
@@ -87,7 +87,7 @@ int runExample() {
 
     path.parseSvgPath("M9 3.881v-3.881l6 6-6 6v-3.966c-6.98-0.164-6.681 4.747-4.904 "
                       "7.966-4.386-4.741-3.455-12.337 4.904-12.119z");
-    path = path.scale(40.0f);
+    path.scale(40.0f);
     canvas.setColor(0xffffffff);
     canvas.line(&path, 0, 0, app.width(), app.height(), 2.0f);
 
