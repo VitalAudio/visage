4443cd91d467c06e7a9912815dae29371ebce423
Matt Tytel <matt@vital.audio>
2025-04-23 13:09:08 -0400

    Path scanline code deduplication.

--- examples/Paths/paths.cpp
+++ examples/Paths/paths.cpp
@@ -24,30 +24,6 @@
 #include <visage/app.h>
 #include <visage/graphics.h>
 
-static constexpr int kStarPoints = 5;
-static constexpr float kPi = 3.14159265358979323846f;
-
-visage::Path drawStar(visage::ApplicationWindow& app, double phase) {
-  visage::Path path;
-  std::complex<float> delta(cos(4.0f * kPi / kStarPoints), sin(4.0f * kPi / kStarPoints));
-  std::complex<float> position(0, -1);
-
-  float center_x = app.width() / 2.0f;
-  float center_y = app.height() / 2.0f;
-
-  for (int i = 0; i < kStarPoints; ++i) {
-    path.lineTo(100.0f * position.real(), 100.0f * position.imag());
-    position = position * delta;
-  }
-
-  path.close();
-  path.rotate(phase);
-  path.translate(center_x, center_y);
-  //path.setFillRule(visage::Path::FillRule::NonZero);
-
-  return path;
-}
-
 inline float randomFloat(float min, float max) {
   static std::random_device random_device;
   static std::mt19937 generator(random_device());
@@ -58,50 +34,29 @@ inline float randomFloat(float min, float max) {
 int runExample() {
   visage::ApplicationWindow app;
 
-  visage::Path star;
-
-  visage::Path path1;
-  visage::Path path2;
+  visage::Path path;
+  path.moveTo(107.989151, 492.898651);
+  path.lineTo(190.269104, 18.6316490);
+  path.lineTo(754.102112, 208.022049);
+  path.lineTo(279.940582, 84.3667450);
+  path.lineTo(708.273193, 63.2528839);
 
   app.onDraw() = [&](visage::Canvas& canvas) {
     canvas.setColor(0xff442233);
     canvas.fill(0, 0, app.width(), app.height());
 
-    std::complex<float> position(cos(-2.0f * kPi / kStarPoints), sin(-2.0f * kPi / kStarPoints));
-
-    // while (true) {
-    //   star.clear();
-    //   star.moveTo(randomFloat(0.0f, app.width()), randomFloat(0.0f, app.height()));
-    //   for (int i = 0; i < 3; ++i)
-    //     star.lineTo(randomFloat(0.0f, app.width()), randomFloat(0.0f, app.height()));
-    //   star.close();
-    //
-    //   star.moveTo(randomFloat(0.0f, app.width()), randomFloat(0.0f, app.height()));
-    //   for (int i = 0; i < 3; ++i)
-    //     star.lineTo(randomFloat(0.0f, app.width()), randomFloat(0.0f, app.height()));
-    //   star.close();
-    //   star.triangulate();
-    // }
-    star.clear();
-    star.moveTo(randomFloat(0.0f, app.width()), randomFloat(0.0f, app.height()));
+    path.clear();
+    path.moveTo(randomFloat(0, app.width()), randomFloat(0, app.height()));
     for (int i = 0; i < 20; ++i)
-      star.lineTo(randomFloat(0.0f, app.width()), randomFloat(0.0f, app.height()));
-    star.close();
+      path.lineTo(randomFloat(0, app.width()), randomFloat(0, app.height()));
+    path.close();
 
-    path2.clear();
-    path2.moveTo(0.3f * app.width(), 0);
-    path2.lineTo(0.3f * app.width(), app.height());
-    path2.lineTo(0.6f * app.width(), app.height());
-    path2.lineTo(0.6f * app.width(), 0);
-    path2.close();
-
-    // star = star.computeXor(path2);
     canvas.setColor(visage::Brush::linear(0xffff00ff, 0xffffff00, visage::Point(0, 0),
                                           visage::Point(app.width(), app.height())));
-    canvas.fill(&star, 0, 0, app.width(), app.height());
+    canvas.fill(&path, 0, 0, app.width(), app.height());
 
     canvas.setColor(0xffffffff);
-    canvas.line(&star, 0, 0, app.width(), app.height(), 3);
+    canvas.line(&path, 0, 0, app.width(), app.height(), 3);
     // app.redraw();
   };
 
