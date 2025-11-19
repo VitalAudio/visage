65aa846441e9ef7d52f55415cfc9ac5b746038bb
Matt Tytel <matt@vital.audio>
2025-11-19 16:37:24 -0500

    Fixing svg clipping paths.

--- visage_ui/svg_frame.cpp
+++ visage_ui/svg_frame.cpp
@@ -23,19 +23,23 @@
 
 namespace visage {
   void SvgFrame::setDimensions() {
-    if (sub_frame_ == nullptr)
-      return;
-
     int m = margin_.compute(dpiScale(), nativeWidth(), nativeHeight(), 0.0f);
     svg_.setDimensions(width() - 2 * m / dpiScale(), height() - 2 * m / dpiScale());
-    sub_frame_->setNativeBounds(m + svg_.drawable()->post_bounding_box.x() * dpiScale(),
-                                m + svg_.drawable()->post_bounding_box.y() * dpiScale(),
-                                nativeWidth() - 2 * m, nativeHeight() - 2 * m);
+
+    if (sub_frame_ == nullptr && svg_.width() && svg_.height()) {
+      sub_frame_ = std::make_unique<SubFrame>(svg_.drawable(), &context_);
+      addChild(sub_frame_.get());
+    }
+
+    if (sub_frame_) {
+      sub_frame_->setNativeBounds(m + svg_.drawable()->post_bounding_box.x() * dpiScale(),
+                                  m + svg_.drawable()->post_bounding_box.y() * dpiScale(),
+                                  nativeWidth() - 2 * m, nativeHeight() - 2 * m);
+    }
   }
 
   void SvgFrame::loadSubFrames() {
-    sub_frame_ = std::make_unique<SubFrame>(svg_.drawable(), &context_);
-    addChild(sub_frame_.get());
+    sub_frame_ = nullptr;
     setDimensions();
   }
 }
