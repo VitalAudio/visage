3391c147eea73bb211d524be086bb93a7e51586c
Matt Tytel <matt@vital.audio>
2025-11-26 09:51:43 -0500

    Reenable more tests.

--- visage_graphics/tests/path_tests.cpp
+++ visage_graphics/tests/path_tests.cpp
@@ -175,152 +175,152 @@ TEST_CASE("Degeneracies", "[graphics]") {
     REQUIRE(screenshot.sample(29, 29).hexRed() == 0);
     REQUIRE(screenshot.sample(10, 10).hexRed() == 0);
   }
-  //
-  // SECTION("Degeneracy rectangle in rectangle middle") {
-  //   Path path;
-  //   path.moveTo(10, 10);
-  //   path.lineTo(40, 10);
-  //   path.lineTo(40, 40);
-  //   path.lineTo(10, 40);
-  //   path.close();
-  //
-  //   path.moveTo(20, 10);
-  //   path.lineTo(30, 10);
-  //   path.lineTo(30, 40);
-  //   path.lineTo(20, 40);
-  //   path.close();
-  //
-  //   Canvas canvas;
-  //   canvas.setWindowless(50, 50);
-  //   canvas.setColor(0xff000000);
-  //   canvas.fill(0, 0, canvas.width(), canvas.height());
-  //   canvas.setColor(0xffff0000);
-  //   canvas.fill(path, 0, 0, kWidth, kWidth);
-  //   canvas.submit();
-  //   const auto& screenshot = canvas.takeScreenshot();
-  //
-  //   REQUIRE(screenshot.sample(10, 10).hexRed() == 0xff);
-  //   REQUIRE(screenshot.sample(10, 25).hexRed() == 0xff);
-  //   REQUIRE(screenshot.sample(10, 39).hexRed() == 0xff);
-  //   REQUIRE(screenshot.sample(21, 10).hexRed() == 0);
-  //   REQUIRE(screenshot.sample(21, 25).hexRed() == 0);
-  //   REQUIRE(screenshot.sample(21, 40).hexRed() == 0);
-  //   REQUIRE(screenshot.sample(29, 10).hexRed() == 0);
-  //   REQUIRE(screenshot.sample(29, 20).hexRed() == 0);
-  //   REQUIRE(screenshot.sample(29, 40).hexRed() == 0);
-  //   REQUIRE(screenshot.sample(31, 10).hexRed() == 0xff);
-  //   REQUIRE(screenshot.sample(31, 20).hexRed() == 0xff);
-  //   REQUIRE(screenshot.sample(31, 39).hexRed() == 0xff);
-  // }
-  //
-  // SECTION("Degeneracy begin point on existing line") {
-  //   Path path;
-  //   path.moveTo(10, 10);
-  //   path.lineTo(40, 10);
-  //   path.lineTo(40, 40);
-  //   path.lineTo(10, 40);
-  //   path.close();
-  //
-  //   path.moveTo(20, 10);
-  //   path.lineTo(30, 0);
-  //   path.lineTo(30, 20);
-  //   path.close();
-  //
-  //   Canvas canvas;
-  //   canvas.setWindowless(50, 50);
-  //   canvas.setColor(0xff000000);
-  //   canvas.fill(0, 0, canvas.width(), canvas.height());
-  //   canvas.setColor(0xffff0000);
-  //   canvas.fill(path, 0, 0, kWidth, kWidth);
-  //   canvas.submit();
-  //   const auto& screenshot = canvas.takeScreenshot();
-  //
-  //   REQUIRE(screenshot.sample(10, 10).hexRed() == 0xff);
-  //   REQUIRE(screenshot.sample(25, 8).hexRed() == 0xff);
-  //   REQUIRE(screenshot.sample(25, 12).hexRed() == 0x00);
-  // }
-  //
-  // SECTION("Degeneracy begin point on two existing lines") {
-  //   Path path;
-  //   path.moveTo(0, 0);
-  //   path.lineTo(0, 20);
-  //   path.lineTo(10, 10);
-  //   path.lineTo(90, 10);
-  //   path.lineTo(100, 20);
-  //   path.lineTo(100, 0);
-  //   path.lineTo(90, 10);
-  //   path.lineTo(10, 10);
-  //   path.close();
-  //
-  //   path.moveTo(20, 10);
-  //   path.lineTo(30, 0);
-  //   path.lineTo(30, 20);
-  //   path.close();
-  //
-  //   Canvas canvas;
-  //   canvas.setWindowless(100, 50);
-  //   canvas.setColor(0xff000000);
-  //   canvas.fill(0, 0, canvas.width(), canvas.height());
-  //   canvas.setColor(0xffff0000);
-  //   canvas.fill(path, 0, 0, kWidth, kWidth);
-  //   canvas.submit();
-  //   const auto& screenshot = canvas.takeScreenshot();
-  //
-  //   REQUIRE(screenshot.sample(5, 10).hexRed() == 0xff);
-  //   REQUIRE(screenshot.sample(15, 10).hexRed() == 0x00);
-  //   REQUIRE(screenshot.sample(25, 10).hexRed() == 0xff);
-  //   REQUIRE(screenshot.sample(35, 10).hexRed() == 0x00);
-  //   REQUIRE(screenshot.sample(95, 10).hexRed() == 0xff);
-  // }
-  //
-  // SECTION("Degeneracy center point star") {
-  //   static constexpr float kPi = 3.14159265358979323846f;
-  //   static constexpr int kStarPoints = 10;
-  //   static constexpr float kRadius = 40.0f;
-  //
-  //   Path star;
-  //   float phase = randomFloat(0.0f, 1.0f);
-  //   std::complex<float> position(std::cos(-2.0f * kPi * phase / kStarPoints),
-  //                                std::sin(-2.0f * kPi * phase / kStarPoints));
-  //
-  //   float center = kWidth * 0.5f;
-  //   star.moveTo(center, center);
-  //   std::complex<float> delta(std::cos(2.0f * kPi / kStarPoints), std::sin(2.0f * kPi / kStarPoints));
-  //
-  //   for (int i = 0; i < kStarPoints; ++i) {
-  //     position = position * delta;
-  //     star.lineTo(center + kRadius * position.real(), center + kRadius * position.imag());
-  //     if (i % 2)
-  //       star.lineTo(center, center);
-  //   }
-  //   star.close();
-  //   Canvas canvas;
-  //   canvas.setWindowless(kWidth, kWidth);
-  //   canvas.setColor(0xff000000);
-  //   canvas.fill(0, 0, kWidth, kWidth);
-  //   canvas.setColor(0xffff0000);
-  //   canvas.fill(star);
-  //   canvas.submit();
-  //   const auto& screenshot = canvas.takeScreenshot();
-  //
-  //   for (int i = 0; i < star.subPaths()[0].points.size() - 2; i += 3) {
-  //     Point p0 = star.subPaths()[0].points[i];
-  //     Point p1 = star.subPaths()[0].points[i + 1];
-  //     Point p2 = star.subPaths()[0].points[i + 2];
-  //     Point inside = (p0 + p1 + p2) / 3.0f;
-  //     Color sample = screenshot.sample(inside);
-  //     REQUIRE(sample.hexRed() == 0xff);
-  //   }
-  //
-  //   for (int i = 2; i < star.subPaths()[0].points.size() - 2; i += 3) {
-  //     Point p0 = star.subPaths()[0].points[i];
-  //     Point p1 = star.subPaths()[0].points[i + 1];
-  //     Point p2 = star.subPaths()[0].points[i + 2];
-  //     Point outside = (p0 + p1 + p2) / 3.0f;
-  //     Color sample = screenshot.sample(outside);
-  //     REQUIRE(sample.hexRed() == 0);
-  //   }
-  // }
+
+  SECTION("Degeneracy rectangle in rectangle middle") {
+    Path path;
+    path.moveTo(10, 10);
+    path.lineTo(40, 10);
+    path.lineTo(40, 40);
+    path.lineTo(10, 40);
+    path.close();
+
+    path.moveTo(20, 10);
+    path.lineTo(30, 10);
+    path.lineTo(30, 40);
+    path.lineTo(20, 40);
+    path.close();
+
+    Canvas canvas;
+    canvas.setWindowless(50, 50);
+    canvas.setColor(0xff000000);
+    canvas.fill(0, 0, canvas.width(), canvas.height());
+    canvas.setColor(0xffff0000);
+    canvas.fill(path, 0, 0, kWidth, kWidth);
+    canvas.submit();
+    const auto& screenshot = canvas.takeScreenshot();
+
+    REQUIRE(screenshot.sample(10, 10).hexRed() == 0xff);
+    REQUIRE(screenshot.sample(10, 25).hexRed() == 0xff);
+    REQUIRE(screenshot.sample(10, 39).hexRed() == 0xff);
+    REQUIRE(screenshot.sample(21, 10).hexRed() == 0);
+    REQUIRE(screenshot.sample(21, 25).hexRed() == 0);
+    REQUIRE(screenshot.sample(21, 40).hexRed() == 0);
+    REQUIRE(screenshot.sample(29, 10).hexRed() == 0);
+    REQUIRE(screenshot.sample(29, 20).hexRed() == 0);
+    REQUIRE(screenshot.sample(29, 40).hexRed() == 0);
+    REQUIRE(screenshot.sample(31, 10).hexRed() == 0xff);
+    REQUIRE(screenshot.sample(31, 20).hexRed() == 0xff);
+    REQUIRE(screenshot.sample(31, 39).hexRed() == 0xff);
+  }
+
+  SECTION("Degeneracy begin point on existing line") {
+    Path path;
+    path.moveTo(10, 10);
+    path.lineTo(40, 10);
+    path.lineTo(40, 40);
+    path.lineTo(10, 40);
+    path.close();
+
+    path.moveTo(20, 10);
+    path.lineTo(30, 0);
+    path.lineTo(30, 20);
+    path.close();
+
+    Canvas canvas;
+    canvas.setWindowless(50, 50);
+    canvas.setColor(0xff000000);
+    canvas.fill(0, 0, canvas.width(), canvas.height());
+    canvas.setColor(0xffff0000);
+    canvas.fill(path, 0, 0, kWidth, kWidth);
+    canvas.submit();
+    const auto& screenshot = canvas.takeScreenshot();
+
+    REQUIRE(screenshot.sample(10, 10).hexRed() == 0xff);
+    REQUIRE(screenshot.sample(25, 8).hexRed() == 0xff);
+    REQUIRE(screenshot.sample(25, 12).hexRed() == 0x00);
+  }
+
+  SECTION("Degeneracy begin point on two existing lines") {
+    Path path;
+    path.moveTo(0, 0);
+    path.lineTo(0, 20);
+    path.lineTo(10, 10);
+    path.lineTo(90, 10);
+    path.lineTo(100, 20);
+    path.lineTo(100, 0);
+    path.lineTo(90, 10);
+    path.lineTo(10, 10);
+    path.close();
+
+    path.moveTo(20, 10);
+    path.lineTo(30, 0);
+    path.lineTo(30, 20);
+    path.close();
+
+    Canvas canvas;
+    canvas.setWindowless(100, 50);
+    canvas.setColor(0xff000000);
+    canvas.fill(0, 0, canvas.width(), canvas.height());
+    canvas.setColor(0xffff0000);
+    canvas.fill(path, 0, 0, kWidth, kWidth);
+    canvas.submit();
+    const auto& screenshot = canvas.takeScreenshot();
+
+    REQUIRE(screenshot.sample(5, 10).hexRed() == 0xff);
+    REQUIRE(screenshot.sample(15, 10).hexRed() == 0x00);
+    REQUIRE(screenshot.sample(25, 10).hexRed() == 0xff);
+    REQUIRE(screenshot.sample(35, 10).hexRed() == 0x00);
+    REQUIRE(screenshot.sample(95, 10).hexRed() == 0xff);
+  }
+
+  SECTION("Degeneracy center point star") {
+    static constexpr float kPi = 3.14159265358979323846f;
+    static constexpr int kStarPoints = 10;
+    static constexpr float kRadius = 40.0f;
+
+    Path star;
+    float phase = randomFloat(0.0f, 1.0f);
+    std::complex<float> position(std::cos(-2.0f * kPi * phase / kStarPoints),
+                                 std::sin(-2.0f * kPi * phase / kStarPoints));
+
+    float center = kWidth * 0.5f;
+    star.moveTo(center, center);
+    std::complex<float> delta(std::cos(2.0f * kPi / kStarPoints), std::sin(2.0f * kPi / kStarPoints));
+
+    for (int i = 0; i < kStarPoints; ++i) {
+      position = position * delta;
+      star.lineTo(center + kRadius * position.real(), center + kRadius * position.imag());
+      if (i % 2)
+        star.lineTo(center, center);
+    }
+    star.close();
+    Canvas canvas;
+    canvas.setWindowless(kWidth, kWidth);
+    canvas.setColor(0xff000000);
+    canvas.fill(0, 0, kWidth, kWidth);
+    canvas.setColor(0xffff0000);
+    canvas.fill(star);
+    canvas.submit();
+    const auto& screenshot = canvas.takeScreenshot();
+
+    for (int i = 0; i < star.subPaths()[0].points.size() - 2; i += 3) {
+      Point p0 = star.subPaths()[0].points[i];
+      Point p1 = star.subPaths()[0].points[i + 1];
+      Point p2 = star.subPaths()[0].points[i + 2];
+      Point inside = (p0 + p1 + p2) / 3.0f;
+      Color sample = screenshot.sample(inside);
+      REQUIRE(sample.hexRed() == 0xff);
+    }
+
+    for (int i = 2; i < star.subPaths()[0].points.size() - 2; i += 3) {
+      Point p0 = star.subPaths()[0].points[i];
+      Point p1 = star.subPaths()[0].points[i + 1];
+      Point p2 = star.subPaths()[0].points[i + 2];
+      Point outside = (p0 + p1 + p2) / 3.0f;
+      Color sample = screenshot.sample(outside);
+      REQUIRE(sample.hexRed() == 0);
+    }
+  }
   //
   // SECTION("Vertical cross line degeneracy") {
   //   Path path;
